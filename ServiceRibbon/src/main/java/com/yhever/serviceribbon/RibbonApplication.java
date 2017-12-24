7f2c794d1cf4a3256df7a34323b60ce25aebae9a
calvin <calvin.ye@capgemini.com>
2017-12-24 10:59:04 +0800

    initial code

---
+++ ServiceRibbon/src/main/java/com/yhever/serviceribbon/RibbonApplication.java
@@ -0,0 +1,32 @@
+package com.yhever.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class RibbonApplication 
+{
+    public static void main( String[] args )
+    {
+    	 SpringApplication.run(RibbonApplication.class, args);
+    }
+    
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
---
+++ ServiceRibbon/src/main/java/com/yhever/serviceribbon/service/HelloService.java
@@ -0,0 +1,17 @@
+package com.yhever.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-USER/welcome?name="+name,String.class);
+    }
+
+}
