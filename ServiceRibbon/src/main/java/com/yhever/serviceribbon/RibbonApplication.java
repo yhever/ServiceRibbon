2423f665d1abb45426437660bc4cd530cb5213cf
calvin <calvin.ye@capgemini.com>
2017-12-25 14:34:04 +0800

    update

--- ServiceRibbon/src/main/java/com/yhever/serviceribbon/RibbonApplication.java
+++ ServiceRibbon/src/main/java/com/yhever/serviceribbon/RibbonApplication.java
@@ -2,9 +2,12 @@ package com.yhever.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +19,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class RibbonApplication 
 {
     public static void main( String[] args )
--- ServiceRibbon/src/main/java/com/yhever/serviceribbon/service/HelloService.java
+++ ServiceRibbon/src/main/java/com/yhever/serviceribbon/service/HelloService.java
@@ -4,14 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class HelloService {
 
-    @Autowired
-    RestTemplate restTemplate;
+	@Autowired
+	RestTemplate restTemplate;
 
-    public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVICE-USER/welcome?name="+name,String.class);
-    }
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String hiService(String name) {
+		return restTemplate.getForObject("http://SERVICE-USER/welcome?name="
+				+ name, String.class);
+	}
+
+	public String hiError(String name) {
+		return "hi," + name + ", sorry, api error!";
+	}
 
 }
